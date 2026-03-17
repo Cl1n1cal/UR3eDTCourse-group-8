de36d40123935645ad29322aad42a66ee71af7ca
Cl1nical <cl1nical@yahoo.com>
2026-03-17 13:52:20 +0100

    Edited functions

--- services/db_recorder_service.py
+++ services/db_recorder_service.py
@@ -6,21 +6,24 @@ from communication.protocol import ROUTING_KEY_RECORDER
 from communication.factory import RabbitMQFactory
 
 class DBRecorderService:
-    def __init__(self, rabbitmq_config, influxdb_config):
+    def __init__(self):
+        self.write_api = None
+        self.influx_db_org = None
+        self.influxdb_bucket = None
+        self.rabbitmq = None
+
+    def read_record_request(self, ch, method, properties, body_json):
+        self.write_api.write(self.influxdb_bucket, self.influx_db_org, body_json)
+
+    def setup(self, influxdb_config):
+        self.rabbitmq = RabbitMQFactory.create_rabbitmq()
+        self.rabbitmq.connect_to_server()
 
         client = InfluxDBClient(**influxdb_config)
         write_api = client.write_api(write_options=SYNCHRONOUS)
         self.write_api = write_api
         self.influx_db_org = influxdb_config["org"]
         self.influxdb_bucket = influxdb_config["bucket"]
-
-        self.rabbitmq: Rabbitmq = RabbitMQFactory.create_rabbitmq()
-
-    def read_record_request(self, ch, method, properties, body_json):
-        self.write_api.write(self.influxdb_bucket, self.influx_db_org, body_json)
-
-    def setup(self):
-        self.rabbitmq.connect_to_server()
 
         self.rabbitmq.subscribe(routing_key=ROUTING_KEY_RECORDER,
                            on_message_callback=self.read_record_request)
