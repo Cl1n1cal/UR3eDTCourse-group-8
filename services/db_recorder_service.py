a3e7be0e33beca387c12db7080e150520849b0bb
Cl1nical <cl1nical@yahoo.com>
2026-03-17 15:15:09 +0100

    Added functionality for writing a robot state to the db

--- services/db_recorder_service.py
+++ services/db_recorder_service.py
@@ -1,9 +1,10 @@
-
+import json
 from influxdb_client.client.influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 from communication.rabbitmq import Rabbitmq
 from communication.protocol import ROUTING_KEY_RECORDER
 from communication.factory import RabbitMQFactory
+from influxdb_client.client.write.point import Point
 
 class DBRecorderService:
     def __init__(self):
@@ -13,7 +14,58 @@ class DBRecorderService:
         self.rabbitmq = None
 
     def read_record_request(self, ch, method, properties, body_json):
-        self.write_api.write(self.influxdb_bucket, self.influx_db_org, body_json)
+        msg = json.loads(body_json)
+                # --- STATE MESSAGE ---
+        if "q_actual" in msg:
+            self.write_state(msg)
+            
+        # --- UNKNOWN ---
+        else:
+            print("Unknown message format:", msg)
+
+    def write_state(self, msg):
+        robot_mode = msg["robot_mode"]
+        q_actual = msg["q_actual"]
+        qd_actual = msg["qd_actual"]
+        q_target = msg["q_target"]
+        timestamp = msg["timestamp"]
+        joint_max_speed = msg["joint_max_speed"]
+        joint_max_acceleration = msg["joint_max_acceleration"]
+        tcp_pose = msg["tcp_pose"]
+
+        point = Point("robotarm_state") \
+            .field("robot_mode", robot_mode) \
+            .field("q_actual_0", q_actual[0]) \
+            .field("q_actual_1", q_actual[1]) \
+            .field("q_actual_2", q_actual[2]) \
+            .field("q_actual_3", q_actual[3]) \
+            .field("q_actual_4", q_actual[4]) \
+            .field("q_actual_5", q_actual[5]) \
+            .field("qd_actual_0", qd_actual[0]) \
+            .field("qd_actual_1", qd_actual[1]) \
+            .field("qd_actual_2", qd_actual[2]) \
+            .field("qd_actual_3", qd_actual[3]) \
+            .field("qd_actual_4", qd_actual[4]) \
+            .field("qd_actual_5", qd_actual[5]) \
+            .field("q_target_0", q_target[0]) \
+            .field("q_target_0", q_target[0]) \
+            .field("q_target_1", q_target[1]) \
+            .field("q_target_2", q_target[2]) \
+            .field("q_target_3", q_target[3]) \
+            .field("q_target_4", q_target[4]) \
+            .field("q_target_5", q_target[5]) \
+            .field("timestamp", timestamp) \
+            .field("joint_max_speed", joint_max_speed) \
+            .field("joint_max_acceleration", joint_max_acceleration) \
+            .field("tcp_pose_x", tcp_pose[0]) \
+            .field("tcp_pose_y", tcp_pose[1]) \
+            .field("tcp_pose_z", tcp_pose[2])
+                        
+       
+        self.write_api.write(self.influxdb_bucket, self.influx_db_org, point)
+
+    def write_control(self, msg):
+        raise NotImplementedError
 
     def setup(self, influxdb_config):
         self.rabbitmq = RabbitMQFactory.create_rabbitmq()
