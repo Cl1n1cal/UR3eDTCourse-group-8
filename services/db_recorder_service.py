438ed973b2f530bfd7165319f5f80c47ab6c3224
Cl1nical <cl1nical@yahoo.com>
2026-03-17 18:17:22 +0100

    Added ability to write to db

--- services/db_recorder_service.py
+++ services/db_recorder_service.py
@@ -2,10 +2,11 @@ import json
 from influxdb_client.client.influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 from communication.rabbitmq import Rabbitmq
-from communication.protocol import ROUTING_KEY_RECORDER
+from communication.protocol import MODEL_ROUTING_KEY_STATE
 from communication.factory import RabbitMQFactory
 from influxdb_client.client.write.point import Point
 import threading
+from datetime import datetime, timezone
 
 class DBRecorderService:
     def __init__(self):
@@ -15,16 +16,19 @@ class DBRecorderService:
         self.rabbitmq = None
 
     def read_record_request(self, ch, method, properties, body_json):
-        msg = json.loads(body_json)
-                # --- STATE MESSAGE ---
+        print("RECORDING REQUEST CALLED ------------------------------------------------")
+        msg = body_json
+        print("STATE MESSAGE WAS:", msg)
+
         if "q_actual" in msg:
             self.write_state(msg)
 
-        # --- UNKNOWN ---
         else:
             print("Unknown message format:", msg)
 
     def write_state(self, msg):
+        print("WRITING STATE TO DB =========================================================")
+        now = datetime.now(timezone.utc)
         robot_mode = msg["robot_mode"]
         q_actual = msg["q_actual"]
         qd_actual = msg["qd_actual"]
@@ -36,32 +40,32 @@ class DBRecorderService:
 
         point = Point("robotarm_state") \
             .field("robot_mode", robot_mode) \
-            .field("q_actual_0", q_actual[0]) \
-            .field("q_actual_1", q_actual[1]) \
-            .field("q_actual_2", q_actual[2]) \
-            .field("q_actual_3", q_actual[3]) \
-            .field("q_actual_4", q_actual[4]) \
-            .field("q_actual_5", q_actual[5]) \
-            .field("qd_actual_0", qd_actual[0]) \
-            .field("qd_actual_1", qd_actual[1]) \
-            .field("qd_actual_2", qd_actual[2]) \
-            .field("qd_actual_3", qd_actual[3]) \
-            .field("qd_actual_4", qd_actual[4]) \
-            .field("qd_actual_5", qd_actual[5]) \
-            .field("q_target_0", q_target[0]) \
-            .field("q_target_0", q_target[0]) \
-            .field("q_target_1", q_target[1]) \
-            .field("q_target_2", q_target[2]) \
-            .field("q_target_3", q_target[3]) \
-            .field("q_target_4", q_target[4]) \
-            .field("q_target_5", q_target[5]) \
-            .field("timestamp", timestamp) \
-            .field("joint_max_speed", joint_max_speed) \
-            .field("joint_max_acceleration", joint_max_acceleration) \
-            .field("tcp_pose_x", tcp_pose[0]) \
-            .field("tcp_pose_y", tcp_pose[1]) \
-            .field("tcp_pose_z", tcp_pose[2])
-                        
+            .field("q_actual_0", float(q_actual[0])) \
+            .field("q_actual_1", float(q_actual[1])) \
+            .field("q_actual_2", float(q_actual[2])) \
+            .field("q_actual_3", float(q_actual[3])) \
+            .field("q_actual_4", float(q_actual[4])) \
+            .field("q_actual_5", float(q_actual[5])) \
+            .field("qd_actual_0", float(qd_actual[0])) \
+            .field("qd_actual_1", float(qd_actual[1])) \
+            .field("qd_actual_2", float(qd_actual[2])) \
+            .field("qd_actual_3", float(qd_actual[3])) \
+            .field("qd_actual_4", float(qd_actual[4])) \
+            .field("qd_actual_5", float(qd_actual[5])) \
+            .field("q_target_0", float(q_target[0])) \
+            .field("q_target_1", float(q_target[1])) \
+            .field("q_target_2", float(q_target[2])) \
+            .field("q_target_3", float(q_target[3])) \
+            .field("q_target_4", float(q_target[4])) \
+            .field("q_target_5", float(q_target[5])) \
+            .field("timestamp_robot_model", float(timestamp)) \
+            .field("joint_max_speed", float(joint_max_speed)) \
+            .field("joint_max_acceleration", float(joint_max_acceleration)) \
+            .field("tcp_pose_x", float(tcp_pose[0])) \
+            .field("tcp_pose_y", float(tcp_pose[1])) \
+            .field("tcp_pose_z", float(tcp_pose[2])) \
+            .time(now)
+                                
        
         self.write_api.write(self.influxdb_bucket, self.influx_db_org, point)
 
@@ -69,6 +73,8 @@ class DBRecorderService:
         raise NotImplementedError
 
     def setup(self, influxdb_config):
+        print("SETUP ################# SETUP########")
+        print("INFLUX CONFIG:", influxdb_config)
         self.rabbitmq = RabbitMQFactory.create_rabbitmq()
         self.rabbitmq.connect_to_server()
 
@@ -77,8 +83,10 @@ class DBRecorderService:
         self.write_api = write_api
         self.influx_db_org = influxdb_config["org"]
         self.influxdb_bucket = influxdb_config["bucket"]
+        print("ORG:", self.influx_db_org)
+        print("BUCKET:", self.influxdb_bucket)
 
-        self.rabbitmq.subscribe(routing_key=ROUTING_KEY_RECORDER,
+        self.rabbitmq.subscribe(routing_key=MODEL_ROUTING_KEY_STATE, # TODO: Change to ROUTING_KEY_RECORDER to catch all robotarm. See communication/protocol.py for details
                         on_message_callback=self.read_record_request)
 
     def start_recording(self):
@@ -86,7 +94,7 @@ class DBRecorderService:
             def run():
                 self.rabbitmq.start_consuming()
 
-            self.thread = threading.Thread(target=run, daemon=True)
+            self.thread = threading.Thread(target=run, daemon=False)
             self.thread.start()
         except KeyboardInterrupt:
             self.rabbitmq.close()
