2955b0d7d22c77d8042d0cb4a8adf978eb54674b
Joao Vera <joaoguedessoaresvera@gmail.com>
2026-04-11 11:01:15 +0200

    small changes to statup config

--- startup/start_calibration_service.py
+++ startup/start_calibration_service.py
@@ -8,7 +8,7 @@ This module starts the simulation service foud in ../services/simulation_service
 def start_calibration_service(ok_queue=None):
     calibration_service = CalibrationService()
     config = load_config("startup/startup.conf")
-    calibration_service.setup(calibration_config=config["calibration_service"])
+    calibration_service.setup(calibration_config=config["digital_twin"]["calibration_service"])
   
     if ok_queue is not None:
         ok_queue.put("OK")
