fe88f3ce59cbeec61c0f3ce92208f607ba4e76cc
Cl1nical <cl1nical@yahoo.com>
2026-04-16 14:11:52 +0200

    major update

--- services/particle_filter_service.py
+++ services/particle_filter_service.py
@@ -8,7 +8,7 @@ from utils.calculation_functions import se3_to_pos_rpy
 from models.robot_model import RobotModel
 from communication.rabbitmq import Rabbitmq
 from communication.factory import RabbitMQFactory, ROUTING_KEY_PARTICLE, ROUTING_KEY_MODEL_STATE, ROUTING_KEY_STATE, ROUTING_KEY_CTRL, ParticleFilterMsgKeys, RobotArmStateKeys, CtrlMsgFields, CtrlMsgKeys, ROUTING_KEY_CALIBRATION
-from communication.protocol import unroll_list
+from communication.protocol import unroll_list, ROUTING_KEY_RECORDER
 from startup.utils.logging_config import create_service_logger
 from nn_folder.classes.robot_prediction_nn import RobotPredictionNN
 
@@ -21,10 +21,13 @@ class ParticleFilterService:
         self.nn_model: RobotPredictionNN = RobotPredictionNN() # Initialized in the setup function
         self.time = start_time
         self.step_size = 0.1
+        self.time_stamp = start_time
         self.q_target = [] # TODO: Set the q_starget when a load program message arrives
         self.particle_filter_count = 0
         self.close_particle_filter_thread = False
         self._l = create_service_logger("particle_filter_service")
+        self.event = threading.Event()
+        self.update_time_counter = 30
 
         # Create the thread for the particle filtering function. Started in the start_serving method. Joined in the cleanup function
         self.particle_filter_thread = threading.Thread(target=self.particle_filter_thread_function, daemon=True)
@@ -40,20 +43,11 @@ class ParticleFilterService:
         self.first_mockup_item_popped = False
 
         # Particle filter parameters
-        self.mockup_noise = 0.00003  # Standard deviation (m) taken from UR3e datasheet: https://www.universal-robots.com/media/1807464/ur3e_e-series_datasheets_web.pdf
-        self.num_particles = 5000
+        self.mockup_noise = 0.04  # Standard deviation (m) taken from UR3e datasheet: https://www.universal-robots.com/media/1807464/ur3e_e-series_datasheets_web.pdf
+        self.N = 100000
         self.particles = []
         self.weights = []
-
-        # Initialize the particle and weight lists (values are overwritten)
-        for i in range(12):
-            self.particles.append(np.random.normal(0, 2, self.num_particles))  # Initialize particles around 0
-            self.weights.append(np.ones(self.num_particles) / self.num_particles)  # Uniform weights
-
-        # Lists to store results
-        self.sim_positions = []
-        self.mockup_measurements = []
-        self.pf_estimates = [0]*12  # Particle filter estimated positions
+        self.pf_estimates = []
 
     def setup(self, particle_filter_config):
         self._l.info("ParticleFilterService service setup with config ", particle_filter_config)
@@ -68,19 +62,19 @@ class ParticleFilterService:
         # Connect rabbitmqs
         self.publisher.connect_to_server()
         self.consumer.connect_to_server()
-
-        # Subscribe to the simulation service
-        self.consumer.subscribe(routing_key=ROUTING_KEY_MODEL_STATE,
-                                on_message_callback=self.read_simulation_state)
         
         # Subscribe to the mockup
         self.consumer.subscribe(routing_key=ROUTING_KEY_STATE,
                                 on_message_callback=self.read_mockup_state)
         
-        # Subscribe to the ctrl messages. Will be used to keep track of the message counting logic by
-        # resetting the message counts when a new control message arrives
-        self.consumer.subscribe(routing_key=ROUTING_KEY_CTRL,
-                                on_message_callback=self.reset_msg_counter)
+        self.consumer.subscribe(routing_key=ROUTING_KEY_RECORDER,
+                                on_message_callback=self.update_time)
+        
+        #self.consumer.subscribe(routing_key=ROUTING_KEY_MODEL_STATE,
+        #                        on_message_callback=self.read_sim_state)
+        
+    def wrap_to_pi(self, x):
+        return (x + np.pi) % (2 * np.pi) - np.pi
     
     def cleanup(self):
         # Close rabbitmqs
@@ -88,16 +82,13 @@ class ParticleFilterService:
         self.publisher.close()
         self.particle_filter_thread.join()
 
-    def upload_state(self):
+    def upload_state(self, time_stamp):
         self._l.debug("Uploading state to RabbitMQ.")
 
         self.time += self.step_size
 
-        rdata = self.create_recorder_state_msg()
-        mdata = self.create_state_msg()
-
-        print("PARTICLE FILTER ########################")
-        print("rdata", rdata)
+        rdata = self.create_recorder_state_msg(time_stamp)
+        mdata = self.create_state_msg(time_stamp)
 
         self.publisher.send_message("robotarm.recorder.particle_filter", rdata)
         self.publisher.send_message(ROUTING_KEY_PARTICLE, mdata)
@@ -130,8 +121,6 @@ class ParticleFilterService:
         timestamp = datetime.fromtimestamp(msg_time, timezone.utc).isoformat()
         fields = {}
         # These might be useful later
-        #fields[RobotArmStateKeys.JOINT_MAX_SPEED] = data[RobotArmStateKeys.JOINT_MAX_SPEED]
-        #fields[RobotArmStateKeys.JOINT_MAX_ACCELERATION] = data[RobotArmStateKeys.JOINT_MAX_ACCELERATION]
 
         fields.update(unroll_list(RobotArmStateKeys.Q_ACTUAL, data[RobotArmStateKeys.Q_ACTUAL]))
         fields.update(unroll_list(RobotArmStateKeys.QD_ACTUAL, data[RobotArmStateKeys.QD_ACTUAL]))
@@ -157,12 +146,14 @@ class ParticleFilterService:
 
         data = self.validate_state_message(message)
         if not data:
+            print("DATA DID NOT EXIST")
             return
         
         # current pos, current vel are necessary for the partcile filter
         # data also contains 'joint_max_speed' and 'joint_max_acceleration' in case they become necesarry
         q_current = data[RobotArmStateKeys.Q_ACTUAL]
         qd_current = data[RobotArmStateKeys.QD_ACTUAL]
+        q_target = data[RobotArmStateKeys.Q_TARGET]
 
         # Append the values to a sinlge list, ready to be given to the nn.predict()
         mockup_val = []
@@ -172,9 +163,184 @@ class ParticleFilterService:
         # Acquire the mutex and update shared mockup msg count and queue
         self.mutex.acquire()
         self.mockup_msg_queue.append(mockup_val)
+        self.q_target = q_target
         self.mutex.release()
+        self.event.set()
     
+
+
+    def particle_filter_thread_function(self):
+        self.event.wait()
+        mean = np.array(self.mockup_msg_queue.pop())
+        std = np.array([0.1]*6 + [0.5]*6)
+
+        self.particles = mean + std * np.random.randn(self.N, 12)
+        self.particles[:, :6] = self.wrap_to_pi(self.particles[:, :6])
+
+        while True:
+            # Check if the thread should be closed
+            if self.close_particle_filter_thread:
+                break
     
+            self.mutex.acquire()
+            try:
+                if len(self.mockup_msg_queue) == 0:
+                    mockup_step = None
+                else:
+                    mockup_step = self.mockup_msg_queue.pop()
+                    q_target = self.q_target.copy()
+                    self.time_stamp = self.time_stamp + self.step_size
+                    time_stamp = self.time_stamp
+            finally:
+                self.mutex.release()
+                time.sleep(0.01)
+
+            if mockup_step != None:
+                # Vectorize q_target
+                q_target_batch = np.tile(q_target, (self.N, 1))
+
+                # --------------------------
+                # 1. VECTORIZE STATE SPLIT
+                # --------------------------
+                joints = self.particles[:, 0:6]
+                velocities = self.particles[:, 6:12]
+
+                # --------------------------
+                # 2. BUILD NN INPUT (BATCH)
+                # --------------------------
+                nn_input = np.concatenate([joints, velocities, q_target_batch], axis=1)
+                nn_input = nn_input.astype(np.float32)
+
+                # --------------------------
+                # 3. NN PREDICTION (BATCH)
+                # --------------------------
+                self.nn_model.predict(nn_input)
+                pred = np.array(self.nn_model.get_prediction())
+
+                # --------------------------
+                # 4. ADD NOISE
+                # --------------------------
+                noise = np.random.normal(0, 0.22, (self.N, 12))
+                self.particles = pred + noise
+
+                # --------------------------
+                # 5. WRAP ANGLES
+                # --------------------------
+                self.particles[:, :6] = self.wrap_to_pi(self.particles[:, :6])
+
+                # --------------------------
+                # 6. MEASUREMENT UPDATE
+                # --------------------------
+                measurement = np.array(mockup_step)
+
+                error = self.particles - measurement
+
+                weights = np.exp(-0.5 * np.sum((error / self.mockup_noise)**2, axis=1))
+                weights += 1e-300
+                weights /= np.sum(weights)
+
+                # --------------------------
+                # 7. RESAMPLING
+                # --------------------------
+                indices = np.random.choice(self.N, self.N, p=weights)
+                self.particles = self.particles[indices]
+
+                # --------------------------
+                # 8. ESTIMATE
+                # --------------------------
+                self.pf_estimates = np.mean(self.particles, axis=0)
+                time_stamp = datetime.fromtimestamp(time_stamp, timezone.utc).isoformat()
+                self.upload_state(time_stamp=time_stamp)
+
+    
+    def create_recorder_state_msg(self, time_stamp):
+        #timestamp = datetime.fromtimestamp(self.time, timezone.utc).isoformat()
+
+        pf_results = self.pf_estimates
+        q_current = []
+        qd_current = []
+
+        # Get the position values
+        for i in range(0, 6):
+            q_current.append(pf_results[i])
+        
+        # Get the speed values
+        for i in range(6, 12):
+            qd_current.append(pf_results[i])
+
+        fields = {}
+
+        fields.update(unroll_list(ParticleFilterMsgKeys.Q_ACTUAL, q_current))
+        fields.update(unroll_list(ParticleFilterMsgKeys.QD_ACTUAL, qd_current))
+
+
+        rdata = {
+            "measurement": "particle_filter",
+            "time": time_stamp,
+            "tags": {
+                "source": "particle_filter_service"
+            },
+            "fields": fields,
+        }
+
+        return rdata
+
+    def create_state_msg(self, time_stamp):
+        #timestamp = datetime.fromtimestamp(self.time, timezone.utc).isoformat()
+        pf_results = self.pf_estimates
+        q_current = []
+        qd_current = []
+
+        # Get the position values
+        for i in range(0, 6):
+            q_current.append(pf_results[i])
+        
+        # Get the speed values
+        for i in range(6, 12):
+            qd_current.append(pf_results[i])
+
+        # Insert values into state message
+        mdata = {
+            ParticleFilterMsgKeys.Q_ACTUAL: q_current,
+            ParticleFilterMsgKeys.QD_ACTUAL: qd_current,
+            ParticleFilterMsgKeys.TIMESTAMP: time_stamp,
+        }
+
+        return mdata
+    
+    def start_serving(self):
+        # Start the particle filter thread
+        self.particle_filter_thread.start()
+
+        try:
+            self.consumer.start_consuming()
+        except KeyboardInterrupt:
+            self._l.info("Particle filter stopped by user.")
+        finally:
+            self.cleanup()
+
+    #def read_sim_state(self, ch, method, properties, message: dict):
+    #    print("got sim msg:", message)
+
+    def update_time(self, ch, method, properties, message: dict):
+        if self.update_time_counter == 30:
+            self.update_time_counter = 0
+
+            time_stamp = message["time"]
+            time_stamp = datetime.fromisoformat(time_stamp).timestamp()
+            measurement = message["measurement"]
+            
+            if time_stamp is None or measurement is None:
+                return
+
+            if measurement == "mockup_state":
+                self.mutex.acquire()
+                self.time_stamp = time_stamp
+                self.mutex.release()
+        else:
+            self.update_time_counter += 1
+
+"""
     def read_simulation_state(self, ch, method, properties, message: dict):
         self._l.debug(f"Received mockup state message: {message}")
 
@@ -199,144 +365,5 @@ class ParticleFilterService:
         self.mutex.acquire()
         self.sim_msg_queue.append(sim_val)
         self.mutex.release()
-    
-    def particle_filter_calculation(self, nn_prediction_list, mockup_vals):
-        # For every joint and velocity
-        for j in range(12):
-            # --- Mockup Measurement (with Noise) ---
-            mockup_joint = mockup_vals[j]
 
-            # nn prediction
-            nn_prediction = nn_prediction_list[j]
-
-            # --- Particle Filter Update ---
-            # 1. Motion Update: Particles drift slightly from simulation prediction
-            self.particles[j] = nn_prediction + np.random.normal(0, 3, self.num_particles)  # Initialize particles around 0
-
-            # 2. Measurement Update: Weight particles based on mockup (noisy measurement)
-            self.weights[j] = np.exp(-0.5 * ((self.particles[j] - mockup_joint) / self.mockup_noise) ** 2)
-            self.weights[j] += 1e-300  # Avoid zeros
-            self.weights[j] /= np.sum(self.weights[j])  # Normalize weights
-
-            # 3. Resampling: Draw new particles based on weights
-            indices = np.random.choice(range(self.num_particles), size=self.num_particles, p=self.weights[j])
-            self.particles[j] = self.particles[j][indices]
-
-            # Store estimated position as the mean of particles
-            self.pf_estimates[j] = np.mean(self.particles[j])
-
-      
-    # 1. Calculate next values
-    # 2. Use the mockup value to calculate with the particle filter
-    # 3. Publish the result
-    def particle_filter_thread_function(self):
-        while True:
-
-            # Check if the thread should be closed
-            if self.close_particle_filter_thread:
-                break
-
-            sim_vals = None
-            mockup_vals = None
-
-            # Lock the shared resources and check values
-            self.mutex.acquire()
-
-            # Adjust the mockup queue by removing the first element that is received
-            if self.first_mockup_item_popped  == False:
-                # Check that the msg queue is not empty
-                if len(self.mockup_msg_queue) != 0:
-                    self.mockup_msg_queue.pop(0)
-                    self.first_mockup_item_popped = True
-            
-            # Get the first item from each of the queues if they are not empty
-            if len(self.mockup_msg_queue) != 0 and len(self.sim_msg_queue) != 0:
-                sim_vals = self.sim_msg_queue.pop(0)
-                mockup_vals = self.mockup_msg_queue.pop(0)
-            
-            # Release the mutex before doing any further computation
-            self.mutex.release()
-
-            # Get the relevant data from the sim_val
-
-
-            # Use the nn to calculate the next position and velocity values
-            if sim_vals != None and mockup_vals != None:
-                self.nn_model.predict(sim_vals)
-                prediction_list = self.nn_model.get_prediction().tolist()
-                self.particle_filter_calculation(prediction_list, mockup_vals)
-                self.upload_state()
-
-
-    # Reset the message counter, ready for new state transitions
-    def reset_msg_counter(self):
-        self.mutex.acquire()
-        self.sim_msg_queue = []
-        self.mockup_msg_queue = []
-        self.q_target = []
-        self.first_mockup_item_popped = False
-        self.mutex.release()
-    
-    def create_recorder_state_msg(self):
-        timestamp = datetime.fromtimestamp(self.time, timezone.utc).isoformat()
-        pf_results = self.pf_estimates
-        q_current = []
-        qd_current = []
-
-        # Get the position values
-        for i in range(0, 6):
-            q_current.append(pf_results[i])
-        
-        # Get the speed values
-        for i in range(6, 12):
-            qd_current.append(pf_results[i])
-
-        fields = {}
-
-        fields.update(unroll_list(ParticleFilterMsgKeys.Q_ACTUAL, q_current))
-        fields.update(unroll_list(ParticleFilterMsgKeys.QD_ACTUAL, qd_current))
-
-        rdata = {
-            "measurement": "particle_filter",
-            "time": timestamp,
-            "tags": {
-                "source": "particle_filter_service"
-            },
-            "fields": fields,
-        }
-
-        return rdata
-
-    def create_state_msg(self):
-        timestamp = datetime.fromtimestamp(self.time, timezone.utc).isoformat()
-        pf_results = self.pf_estimates
-        q_current = []
-        qd_current = []
-
-        # Get the position values
-        for i in range(0, 6):
-            q_current.append(pf_results[i])
-        
-        # Get the speed values
-        for i in range(6, 12):
-            qd_current.append(pf_results[i])
-
-        # Insert values into state message
-        mdata = {
-            ParticleFilterMsgKeys.Q_ACTUAL: q_current,
-            ParticleFilterMsgKeys.QD_ACTUAL: qd_current,
-            ParticleFilterMsgKeys.TIMESTAMP: timestamp,
-        }
-
-        return mdata
-    
-    def start_serving(self):
-        # Start the particle filter thread
-        self.particle_filter_thread.start()
-
-        try:
-            self.consumer.start_consuming()
-        except KeyboardInterrupt:
-            self._l.info("Particle filter stopped by user.")
-        finally:
-            self.cleanup()
+"""
